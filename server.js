72f0cf3c517ac8275eaa2cdcbcc2f04f136ee1bf
Guilherme <guilhermebreda0488@gmail.com>
2018-08-19 00:23:08 -0300

    Primeiro commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+require('dotenv').config()
+const port = process.env.PORT || 3002;
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+      console.log("Connected to the database");
+  })
+  .catch(err => {
+      console.log("Could not connect to the database");
+      process.exit();
+  });
+
+app.get('/', (req, res) => {
+    res.json({"message":"Welcome!"})
+});
+
+require('./app/routes/note.routes.js')(app);
+
+app.listen(port, () => {
+    console.log("Server is up");
+});
